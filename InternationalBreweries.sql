c494683fc8101190ef43c471501e88a2e702193e
azeezah08 <117105257+azeezah08@users.noreply.github.com>
2025-05-18 16:35:47 +0100

    Update InternationalBreweries.sql

--- InternationalBreweries.sql
+++ InternationalBreweries.sql
@@ -79,16 +79,96 @@ Order by 2 Desc
 
 
 /*Within the last two years, the brand manager wants to know the top three brands
-consumed in the francophone countries*/Select top 3 brands, SUM(quantity) as TotalQuantity
+consumed in the francophone countries*/
+
+Select top 3 brands, SUM(quantity) as TotalQuantity
 From InternationalBreweries..International_Breweries$
 Where countries in ('Senegal','Togo','Benin') AND years in (2018,2019)
 Group by brands
 Order by 2 Desc
 
---Find out the top two choice of consumer brands in GhanaSelect top 2 brands, SUM(quantity) as TotalQuantity
+--Find out the top two choice of consumer brands in Ghana
+Select top 2 brands, SUM(quantity) as TotalQuantity
 From InternationalBreweries..International_Breweries$
 Where countries = 'Ghana'
 Group by brands
 Order by 2 Desc
-/* Find out the details of beers consumed in the past three years in the most oil reached
-country in West Africa.*/--the brandsSelect Distinct brandsFrom InternationalBreweries..International_Breweries$-- beta malt and grand malt are not beers-- Nigeria is the most oil rich country in west africaSelect *From InternationalBreweries..International_Breweries$Where NOT brands in ('beta malt','grand malt') AND countries = 'Nigeria'/*Favorites malt brand in Anglophone region between 2018 and 2019*/Select top 1 brands, SUM(quantity)From InternationalBreweries..International_Breweries$Where brands in ('beta malt','grand malt') AND countries in ('Nigeria','Ghana') AND years in (2018, 2019)Group by brandsorder by 2 DESC/* Which brands sold the highest in 2019 in Nigeria*/Select top 1 brands, SUM(quantity)From InternationalBreweries..International_Breweries$Where countries = 'Nigeria' AND years =2019Group by brandsorder by 2 DESC/*Favorites brand in South_South region in Nigeria*/Select top 1  brands, SUM(quantity)From InternationalBreweries..International_Breweries$Where region = 'Southsouth' AND countries = 'Nigeria'Group by brandsOrder by 2 DESC/*Beer consumption in nigeria*/Select brands, SUM(Quantity)From InternationalBreweries..International_Breweries$Where NOT brands in ('beta malt','grand malt') AND countries = 'Nigeria'Group by brands/* Level of consumption of Budweiser in the regions in Nigeria*/Select region, SUM(quantity)From InternationalBreweries..International_Breweries$Where brands = 'budweiser' AND countries = 'Nigeria'Group by region/*Level of consumption of Budweiser in the regions in Nigeria in 2019 (Decision on Promo)*/Select region, SUM(quantity)From InternationalBreweries..International_Breweries$Where brands = 'budweiser' AND countries = 'Nigeria' AND years = 2019Group by region /* Country with the highest consumption of beer*/Select top 1 Countries, SUM(Quantity)From InternationalBreweries..International_Breweries$Where NOT brands in ('beta malt','grand malt') Group by countriesOrder by 2 DESC --Highest sales personnel of Budweiser in Senegal Select  top 1 sales_rep, SUM(Quantity)From InternationalBreweries..International_Breweries$Where brands = 'budweiser' AND countries = 'Senegal'Group by Sales_repOrder by 2 desc--Country with the highest profit of the fourth quarter in 2019Select top 1 countries, SUM(profit)From InternationalBreweries..International_Breweries$Where months in ('october','november','december') AND years = 2019Group by countriesorder by 2 Desc
+
+/* Find out the details of beers consumed in the past three years in the most oil reached
+country in West Africa.*/
+
+--the brands
+Select Distinct brands
+From InternationalBreweries..International_Breweries$
+
+-- beta malt and grand malt are not beers
+-- Nigeria is the most oil rich country in west africa
+
+Select *
+From InternationalBreweries..International_Breweries$
+Where NOT brands in ('beta malt','grand malt') AND countries = 'Nigeria'
+
+/*Favorites malt brand in Anglophone region between 2018 and 2019*/
+Select top 1 brands, SUM(quantity)
+From InternationalBreweries..International_Breweries$
+Where brands in ('beta malt','grand malt') AND countries in ('Nigeria','Ghana') AND years in (2018, 2019)
+Group by brands
+order by 2 DESC
+
+/* Which brands sold the highest in 2019 in Nigeria*/
+
+Select top 1 brands, SUM(quantity)
+From InternationalBreweries..International_Breweries$
+Where countries = 'Nigeria' AND years =2019
+Group by brands
+order by 2 DESC
+
+/*Favorites brand in South_South region in Nigeria*/
+
+Select top 1  brands, SUM(quantity)
+From InternationalBreweries..International_Breweries$
+Where region = 'Southsouth' AND countries = 'Nigeria'
+Group by brands
+Order by 2 DESC
+
+/*Beer consumption in nigeria*/
+Select brands, SUM(Quantity)
+From InternationalBreweries..International_Breweries$
+Where NOT brands in ('beta malt','grand malt') AND countries = 'Nigeria'
+Group by brands
+
+/* Level of consumption of Budweiser in the regions in Nigeria*/
+
+Select region, SUM(quantity)
+From InternationalBreweries..International_Breweries$
+Where brands = 'budweiser' AND countries = 'Nigeria'
+Group by region
+
+/*Level of consumption of Budweiser in the regions in Nigeria in 2019 (Decision on Promo)*/
+Select region, SUM(quantity)
+From InternationalBreweries..International_Breweries$
+Where brands = 'budweiser' AND countries = 'Nigeria' AND years = 2019
+Group by region 
+
+/* Country with the highest consumption of beer*/
+Select top 1 Countries, SUM(Quantity)
+From InternationalBreweries..International_Breweries$
+Where NOT brands in ('beta malt','grand malt') 
+Group by countries
+Order by 2 DESC
+
+ --Highest sales personnel of Budweiser in Senegal
+
+ Select  top 1 sales_rep, SUM(Quantity)
+From InternationalBreweries..International_Breweries$
+Where brands = 'budweiser' AND countries = 'Senegal'
+Group by Sales_rep
+Order by 2 desc
+
+--Country with the highest profit of the fourth quarter in 2019
+
+Select top 1 countries, SUM(profit)
+From InternationalBreweries..International_Breweries$
+Where months in ('october','november','december') AND years = 2019
+Group by countries
+order by 2 Desc
